6aba28bfcda520731820a8562b199d18c9199448
Jimmy Ronaldo Macedo Pizango <jimmy.rmp@hotmail.com>
2023-09-25 23:34:55 -0500

    Fix - fx/day1_viewandlist_JRMP - Vista personalizada de la lista

---
+++ iDane/iDane/ItemRow.swift
@@ -0,0 +1,20 @@
+//
+//  ItemRow.swift
+//  iDane
+//
+//  Created by Whiz on 25/09/23.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow()
+    }
+}
