ae9bf2f05a66a2fbdfe68293fae4c774fa8d6e64
Elver <holahacker01@gmail.com>
2023-09-25 21:56:52 -0500

    IDANI_PROJECT

---
+++ iDane/iDane/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iDane
+//
+//  Created by Elver Mayta Hernández on 23/09/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        VStack{
+            Text("Hola Mundo")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDane/iDane/iDaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDaneApp.swift
+//  iDane
+//
+//  Created by Elver Mayta Hernández on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct iDaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
