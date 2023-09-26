60981051a2bcc1256c7d3029399027274129bfa5
Elver <holahacker01@gmail.com>
2023-09-25 23:01:40 -0500

    menu

--- iDane/iDane/ContentView.swift
+++ iDane/iDane/ContentView.swift
@@ -9,11 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
+    let menu = Bundle.main.decode([MenuSection].self, from: "menu.json")
     
     var body: some View {
-        VStack{
-            Text("Hola Mundo")
-        }
+        NavigationView{
+                    List{
+                        ForEach(menu) { section in
+                            Section(header: Text(section.name)){
+                                ForEach(section.items) { item in
+                                    Text(item.name)
+                                }
+                            }
+                        }
+                    }
+                    .navigationTitle("Menu")
+                    .listStyle(GroupedListStyle())
+                }
     }
 }
 
