3d43072762ac763e24ccc69225fceceb7c7c355f
Jimmy Ronaldo Macedo Pizango <jimmy.rmp@hotmail.com>
2023-09-25 23:58:54 -0500

    Fix - fx/day1_viewandlist_JRMP - Vista personalizada de la lista actualizada

--- iDane/iDane/ItemRow.swift
+++ iDane/iDane/ItemRow.swift
@@ -8,13 +8,22 @@
 import SwiftUI
 
 struct ItemRow: View {
+    let item: MenuItem
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack{
+            Image(item.thumbnailImage)
+            
+            VStack (alignment: .leading){
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+        
     }
 }
 
 struct ItemRow_Previews: PreviewProvider {
     static var previews: some View {
-        ItemRow()
+        ItemRow(item: MenuItem.example)
     }
 }
