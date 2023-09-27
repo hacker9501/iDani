c0548ea7e1dd6340425e140d03b9f4509fffa604
Elver <holahacker01@gmail.com>
2023-09-26 22:47:24 -0500

    uso de fonts and colors

--- iDane/iDane/ItemRow.swift
+++ iDane/iDane/ItemRow.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ItemRow: View {
     let item: MenuItem
+    
+    let colors: [String: Color] = ["D": .purple, "G": .black, "N": .red,"S": .blue, "V": .green]
+    
     var body: some View {
         HStack{
             Image(item.thumbnailImage)
+                .clipShape(Circle())
+                .overlay(Circle().stroke(.gray, lineWidth: 2))
             
             VStack (alignment: .leading){
                 Text(item.name)
+                    .font(.headline)
                 Text("$\(item.price)")
+            }
+            Spacer()
+            
+            ForEach(item.restrictions, id: \.self){restriction in Text(restriction)
+                    .font(.caption)
+                    .fontWeight(.black)
+                    .padding(5)
+                    .background(colors[restriction,default: .black])
+                    .clipShape(Circle())
+                    .foregroundColor(.white)
+                
             }
         }
         
