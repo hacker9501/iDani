531afb16f1835272cd12ea24a9ab90225c3c7df1
Elver <holahacker01@gmail.com>
2023-09-28 21:49:12 -0500

    uso de los objects

--- iDane/iDane/iDaneApp.swift
+++ iDane/iDane/iDaneApp.swift
@@ -9,9 +9,13 @@ import SwiftUI
 
 @main
 struct iDaneApp: App {
+    
+    @StateObject var order = Order()
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(order)
         }
     }
 }
