b15335d44e7f26f67f8a72d3ff3318e34a8384a2
Jimmy Ronaldo Macedo Pizango <jimmy.rmp@hotmail.com>
2023-10-06 00:39:20 -0500

    FIX: Se corrige la muestra del tab_view en las vistas
    - Se añadió el contenedor principal del tab_view en la raíz del proyecto

--- iDane/iDane/iDaneApp.swift
+++ iDane/iDane/iDaneApp.swift
@@ -14,7 +14,7 @@ struct iDaneApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
                 .environmentObject(order)
         }
     }
