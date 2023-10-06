8ad79ce89ca4f0ed689b37971a09cb810bd8e68a
Jimmy Ronaldo Macedo Pizango <jimmy.rmp@hotmail.com>
2023-10-06 00:26:32 -0500

    FEATURE: Se agrega el precio total
    - Se añadió una variable calculada totalPrice que retorna el precio
    - Se usó interpolación de cadena para formatear el resultado con signo de moneda USD
    - Se modificó la section con la variable totalPrice

--- iDane/iDane/CheckoutView.swift
+++ iDane/iDane/CheckoutView.swift
@@ -18,6 +18,12 @@ struct CheckoutView: View {
     let tipAmounts = [10, 15, 20, 25, 0]
     @State private var tipAmount = 15
     
+    var totalPrice: String {
+        let total = Double(order.total)
+        let tipValue = total / 100 * Double(tipAmount)
+        return (total + tipValue).formatted(.currency(code: "USD"))
+    }
+    
     var body: some View {
         Form {
             Section {
@@ -39,7 +45,7 @@ struct CheckoutView: View {
                 }
                 .pickerStyle(.segmented)
             }
-            Section("Total: $100") {
+            Section("Total: \(totalPrice)") {
                 Button("Confirm order") {
                     // place the order
                 }
